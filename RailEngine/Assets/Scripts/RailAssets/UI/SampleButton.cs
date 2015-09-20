5d7bc542a6c5455243e7725d2c54f9b9affbba2c
deluxturtle <sebaturtle@gmail.com>
2015-09-19 19:55:50 -0500

    Level Select Menu Basics Down!!
    Got a level Selector menu functional.
    TODO: somehow get the levels to be read in from a folder

---
+++ RailEngine/Assets/Scripts/RailAssets/UI/SampleButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+    public Button button;
+    public Text authorLabel;
+    public Image icon;
+    public Text levelName;
+    
+}
