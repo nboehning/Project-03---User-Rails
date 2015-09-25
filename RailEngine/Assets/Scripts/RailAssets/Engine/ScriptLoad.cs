dd7c51ca7924f6017962804d2e9a2d023facb836
deluxturtle <sebaturtle@gmail.com>
2015-09-25 09:17:06 -0500

    Testing reading files.

--- RailEngine/Assets/Scripts/RailAssets/Engine/ScriptLoad.cs
+++ RailEngine/Assets/Scripts/RailAssets/Engine/ScriptLoad.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.IO;
 using System.Collections.Generic;
@@ -8,23 +9,65 @@ using System.Collections.Generic;
 /// </summary>
 public class ScriptLoad : MonoBehaviour {
 
+    public Text textConsole;
+
     TextAsset textFile;
     TextReader reader;
 
     DirectoryInfo info;
 
+    //for testing to see if levels are there
+    List<string> levelNames = new List<string>();
+
+    //read in the levels into this
+    List<Item> levels = new List<Item>();
 
     void Start()
     {
         //read in files in a directory
-
         info = new DirectoryInfo(Application.dataPath + "/");
         FileInfo[] levelInfo = info.GetFiles();
 
         foreach(FileInfo file in levelInfo)
         {
-            Debug.Log(file);
+            if (file.Name.EndsWith(".dan"))
+            {
+                levelNames.Add(file.Name.ToString());
+                textConsole.text += "\n" + file;
+
+                reader = file.OpenText();
+
+
+                string lineOfText;
+                int lineNumber = 0;
+                Item tempItem = new Item();
+                while ((lineOfText = reader.ReadLine()) != null)
+                {
+
+                    
+                    switch (lineNumber)
+                    {
+                        case 0:
+                            tempItem.author = lineOfText;
+                            break;
+                        case 1:
+                            tempItem.name = lineOfText;
+                            break;
+                            //ADD tempItem To a list so that the scroll list will read it.
+                    }
+
+                    lineNumber++;
+                }
+
+            }
         }
+
+        if(levelNames.Count <= 0)
+        {
+            textConsole.text += "\nNo Levels Found in <" + Application.dataPath + "/>";
+        }
+
+
 
     }
 
