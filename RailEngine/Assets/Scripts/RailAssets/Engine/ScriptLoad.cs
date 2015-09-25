65af4201d54649ae3e275410c236a34fb6c216f3
deluxturtle <sebaturtle@gmail.com>
2015-09-24 22:41:13 -0500

    started trying to read in files

---
+++ RailEngine/Assets/Scripts/RailAssets/Engine/ScriptLoad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Andrew Seba
+/// </summary>
+public class ScriptLoad : MonoBehaviour {
+
+    TextAsset textFile;
+    TextReader reader;
+
+    DirectoryInfo info;
+
+
+    void Start()
+    {
+        //read in files in a directory
+
+        info = new DirectoryInfo(Application.dataPath + "/");
+        FileInfo[] levelInfo = info.GetFiles();
+
+        foreach(FileInfo file in levelInfo)
+        {
+            Debug.Log(file);
+        }
+
+    }
+
+
+    
+	
+}
