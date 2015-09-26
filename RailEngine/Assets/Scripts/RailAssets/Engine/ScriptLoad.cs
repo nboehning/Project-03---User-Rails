c82e639a09d0b4af81488616b96bc43ca7d12669
deluxturtle <sebaturtle@gmail.com>
2015-09-26 12:04:03 -0500

    buttons are getting its data back! woo!
    Now on to importing the level to a new or current scene.

--- RailEngine/Assets/Scripts/RailAssets/Engine/ScriptLoad.cs
+++ RailEngine/Assets/Scripts/RailAssets/Engine/ScriptLoad.cs
@@ -9,69 +9,73 @@ using System.Collections.Generic;
 /// </summary>
 public class ScriptLoad : MonoBehaviour {
 
-    public Text textConsole;
+	public Text textConsole;
 
-    TextAsset textFile;
-    TextReader reader;
+	TextAsset textFile;
+	TextReader reader;
 
-    DirectoryInfo info;
+	DirectoryInfo info;
 
-    //for testing to see if levels are there
-    List<string> levelNames = new List<string>();
+	//for testing to see if levels are there
+	List<string> levelNames = new List<string>();
 
-    //read in the levels into this
-    List<Item> levels = new List<Item>();
+	//read in the levels into this
+	public List<Item> levels = new List<Item>();
 
-    void Start()
-    {
-        //read in files in a directory
-        info = new DirectoryInfo(Application.dataPath + "/");
-        FileInfo[] levelInfo = info.GetFiles();
+	void Start()
+	{
+		//read in files in a directory
+		info = new DirectoryInfo(Application.dataPath + "/");
+		FileInfo[] levelInfo = info.GetFiles();
 
-        foreach(FileInfo file in levelInfo)
-        {
-            if (file.Name.EndsWith(".dan"))
-            {
-                levelNames.Add(file.Name.ToString());
-                textConsole.text += "\n" + file;
+		foreach(FileInfo file in levelInfo)
+		{
+			if (file.Name.EndsWith(".dan"))
+			{
+				levelNames.Add(file.Name.ToString());
+				textConsole.text += "\n" + file;
 
-                reader = file.OpenText();
-
-
-                string lineOfText;
-                int lineNumber = 0;
-                Item tempItem = new Item();
-                while ((lineOfText = reader.ReadLine()) != null)
-                {
-
-                    
-                    switch (lineNumber)
-                    {
-                        case 0:
-                            tempItem.author = lineOfText;
-                            break;
-                        case 1:
-                            tempItem.name = lineOfText;
-                            break;
-                            //ADD tempItem To a list so that the scroll list will read it.
-                    }
-
-                    lineNumber++;
-                }
-
-            }
-        }
-
-        if(levelNames.Count <= 0)
-        {
-            textConsole.text += "\nNo Levels Found in <" + Application.dataPath + "/>";
-        }
+				reader = file.OpenText();
 
 
+				string lineOfText;
+				int lineNumber = 0;
+				Item tempItem = new Item();
+				while ((lineOfText = reader.ReadLine()) != null)
+				{
 
-    }
+					
+					switch (lineNumber)
+					{
+						case 0:
+							tempItem.author = lineOfText;
+							break;
+						case 1:
+							tempItem.name = lineOfText;
+							break;
+							
+					}
+
+					lineNumber++;
+				}
+
+				levels.Add(tempItem);
+
+			}
+		}
+
+		if(levelNames.Count <= 0)
+		{
+			textConsole.text += "\nNo Levels Found in <" + Application.dataPath + "/>"; 
+		}
+
+		//BroadcastMessage("LoadInLevelList");
+        ScriptCreateScrollList scrollList = gameObject.GetComponent<ScriptCreateScrollList>();
+        scrollList.LoadInLevelList();
+
+	}
 
 
-    
+	
 	
 }
---
+++ RailEngine/Assets/Scripts/RailAssets/UI/ScriptLoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScriptLoadLevel : MonoBehaviour {
+
+	public void _LoadLevel()
+	{
+		
+	}
+}
